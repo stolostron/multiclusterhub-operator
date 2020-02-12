688b231bf8fe75d76af6b5de1c832fbda68b5913
zkayyali812 <zkayyali812@gmail.com>
2020-02-11 20:23:04 -0500

    remove from makefile add to script

---
+++ common/scripts/build_image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+
+CONTAINER_ENGINE=$1
+REGISTRY=$2
+IMG=$3
+VERSION=$4
+
+sudo operator-sdk build --image-builder $(CONTAINER_ENGINE) $(REGISTRY)/$(IMG):$(VERSION) --verbose 
+@$(CONTAINER_ENGINE) push $(REGISTRY)/$(IMG):$(VERSION)
