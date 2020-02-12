33d72af138d4cd9f6979283f7a6c996af9c53b1d
zkayyali812 <zkayyali812@gmail.com>
2020-02-11 20:39:02 -0500

    add docker login

--- common/scripts/build_image.sh
+++ common/scripts/build_image.sh
@@ -7,5 +7,9 @@ REGISTRY=$2
 IMG=$3
 VERSION=$4
 
+echo $DOCKER_USER
+
+docker login "REGISTRY" -u "$DOCKER_USER" -p "$DOCKER_PASS"
+
 operator-sdk build --image-builder "$CONTAINER_ENGINE" "$REGISTRY"/"$IMG":"$VERSION" --verbose 
 "$CONTAINER_ENGINE" push "$REGISTRY"/"$IMG":"$VERSION"
