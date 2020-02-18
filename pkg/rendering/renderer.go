7e6037cd9f58db2a28f4a081fa4bee3965c054c4
Jakob Gray <jakobgray@ibm.com>
2020-02-18 09:34:34 -0500

    Add repo component as watched deployment

--- pkg/rendering/renderer.go
+++ pkg/rendering/renderer.go
@@ -17,6 +17,7 @@ const (
 	controllerName        = "mcm-controller"
 	webhookName           = "webhook-core-webhook"
 	clusterControllerName = "multicloud-operators-cluster-controller"
+	helmRepoName					= "multicloudhub-repo"
 	metadataErr           = "failed to find metadata field"
 )
 
@@ -119,6 +120,8 @@ func (r *Renderer) renderDeployments(res *resource.Resource) (*unstructured.Unst
 	case webhookName:
 		return &unstructured.Unstructured{Object: res.Map()}, nil
 	case clusterControllerName:
+		return &unstructured.Unstructured{Object: res.Map()}, nil
+	case helmRepoName:
 		return &unstructured.Unstructured{Object: res.Map()}, nil
 	default:
 		return nil, fmt.Errorf("unknown MultipleCloudHub deployment component %s", name)
