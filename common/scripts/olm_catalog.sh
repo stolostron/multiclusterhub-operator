0a215d04f31f9344011e2c081419d73ca2cd338a
zkayyali812 <zkayyali812@gmail.com>
2020-03-10 09:51:47 -0400

    replaces replacment with rhibmcollab insteaddd of open-cluster-mgmt

--- common/scripts/olm_catalog.sh
+++ common/scripts/olm_catalog.sh
@@ -147,9 +147,9 @@ EOF
   docker push $_IMAGE_REFERENCE
   _SHA_IMAGE_REFERENCE=$(docker inspect --format='{{index .RepoDigests 0}}' $_IMAGE_REFERENCE)
   if [ "$(uname)" = "Darwin" ]; then
-    sed -i "" "s|${_IMAGE_REFERENCE}|${_SHA_IMAGE_REFERENCE}|g" "${OLMOUTPUTDIR}"/multicloudhub.csv.yaml
+    sed -i "" "s|quay.io/rhibmcollab/multicloudhub-operator:latest|${_SHA_IMAGE_REFERENCE}|g" "${OLMOUTPUTDIR}"/multicloudhub.csv.yaml
   else
-    sed -i "s|${_IMAGE_REFERENCE}|${_SHA_IMAGE_REFERENCE}|g" "${OLMOUTPUTDIR}"/multicloudhub.csv.yaml
+    sed -i "s|quay.io/rhibmcollab/multicloudhub-operator:latest|${_SHA_IMAGE_REFERENCE}|g" "${OLMOUTPUTDIR}"/multicloudhub.csv.yaml
   fi
 
   cat "${OLMOUTPUTDIR}"/multicloudhub.csv.yaml
