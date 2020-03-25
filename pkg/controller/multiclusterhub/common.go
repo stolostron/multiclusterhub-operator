193332f94aa2bed7fcf8edb8a55b84ad23052941
Jakob Gray <jdg7sh@virginia.edu>
2020-03-25 15:12:13 -0400

    Wait for CRD availability (#130)
    * Create helmrepo, channel, and cert-manager subscriptions programmatically in reconcile loop
    
    * remove parts from the kustomize folders
    
    * fix dynamic client name
    
    * Less verbose error on benign error due to out of date resource version
    
    * refactor
    
    * hide dynamic client gen logic from reconciler function
    
    * Remove inclusterconfig function from ingress domain discovery function
    
    * Log errors setting controller reference; reorganize file locations
    
    * remove unused import
    
    * Reduce code smells by removing logging duplication
    
    * Initialize separate variable for custom logs
    
    * Handle object modifed error as expected behavior when updating status
    
    * Initialize new var properly
    
    Co-authored-by: Jakob Gray <jakobgray@ibm.com>

---
+++ pkg/controller/multiclusterhub/common.go
@@ -0,0 +1,149 @@
+package multiclusterhub
+
+import (
+	"context"
+	"time"
+
+	operatorsv1alpha1 "github.com/open-cluster-management/multicloudhub-operator/pkg/apis/operators/v1alpha1"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/dynamic"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+func (r *ReconcileMultiClusterHub) ensureDeployment(m *operatorsv1alpha1.MultiClusterHub, dep *appsv1.Deployment) (*reconcile.Result, error) {
+	dplog := log.WithValues("Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+
+	// See if deployment already exists and create if it doesn't
+	found := &appsv1.Deployment{}
+	err := r.client.Get(context.TODO(), types.NamespacedName{
+		Name:      dep.Name,
+		Namespace: m.Namespace,
+	}, found)
+	if err != nil && errors.IsNotFound(err) {
+
+		// Create the deployment
+		err = r.client.Create(context.TODO(), dep)
+		if err != nil {
+			// Deployment failed
+			dplog.Error(err, "Failed to create new Deployment")
+			return &reconcile.Result{}, err
+		}
+
+		// Deployment was successful
+		dplog.Info("Created a new Deployment")
+		return nil, nil
+
+	} else if err != nil {
+		// Error that isn't due to the deployment not existing
+		dplog.Error(err, "Failed to get Deployment")
+		return &reconcile.Result{}, err
+	}
+
+	return nil, nil
+}
+
+func (r *ReconcileMultiClusterHub) ensureService(m *operatorsv1alpha1.MultiClusterHub, s *corev1.Service) (*reconcile.Result, error) {
+	svlog := log.WithValues("Service.Namespace", s.Namespace, "Service.Name", s.Name)
+
+	found := &corev1.Service{}
+	err := r.client.Get(context.TODO(), types.NamespacedName{
+		Name:      s.Name,
+		Namespace: m.Namespace,
+	}, found)
+	if err != nil && errors.IsNotFound(err) {
+
+		// Create the service
+		err = r.client.Create(context.TODO(), s)
+
+		if err != nil {
+			// Creation failed
+			svlog.Error(err, "Failed to create new Service")
+			return &reconcile.Result{}, err
+		}
+
+		// Creation was successful
+		svlog.Info("Created a new Service")
+		return nil, nil
+
+	} else if err != nil {
+		// Error that isn't due to the service not existing
+		svlog.Error(err, "Failed to get Service")
+		return &reconcile.Result{}, err
+	}
+
+	return nil, nil
+}
+
+func (r *ReconcileMultiClusterHub) ensureSecret(m *operatorsv1alpha1.MultiClusterHub, s *corev1.Secret) (*reconcile.Result, error) {
+	selog := log.WithValues("Secret.Namespace", s.Namespace, "Secret.Name", s.Name)
+
+	found := &corev1.Secret{}
+	err := r.client.Get(context.TODO(), types.NamespacedName{
+		Name:      s.Name,
+		Namespace: m.Namespace,
+	}, found)
+	if err != nil && errors.IsNotFound(err) {
+
+		// Create the secret
+		err = r.client.Create(context.TODO(), s)
+		if err != nil {
+			// Creation failed
+			selog.Error(err, "Failed to create new Secret")
+			return &reconcile.Result{}, err
+		}
+
+		// Creation was successful
+		selog.Info("Created a new secret")
+		return nil, nil
+
+	} else if err != nil {
+		// Error that isn't due to the secret not existing
+		selog.Error(err, "Failed to get Secret")
+		return &reconcile.Result{}, err
+	}
+
+	return nil, nil
+}
+
+func createDynamicClient() (dynamic.Interface, error) {
+	config, err := config.GetConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	dynClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return dynClient, err
+}
+
+func (r *ReconcileMultiClusterHub) apiReady(gv schema.GroupVersion) (*reconcile.Result, error) {
+	cfg, err := config.GetConfig()
+	if err != nil {
+		log.Error(err, "Failed to create rest config")
+		return &reconcile.Result{}, err
+	}
+
+	c, err := discovery.NewDiscoveryClientForConfig(cfg)
+	if err != nil {
+		log.Error(err, "Failed to create discovery client")
+		return &reconcile.Result{}, err
+	}
+
+	err = discovery.ServerSupportsVersion(c, gv)
+	if err != nil {
+		// Wait a little and try again
+		log.Info("Waiting for API group to be available", "API group", gv)
+		return &reconcile.Result{RequeueAfter: time.Second * 10}, nil
+	}
+	return nil, nil
+}
