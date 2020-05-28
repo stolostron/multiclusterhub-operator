2024fdb8e8c33ff532bc1ca062f92804c029a75d
Jakob Gray <jdg7sh@virginia.edu>
2020-05-28 12:42:26 -0400

    Add variable for requeue time

--- pkg/controller/multiclusterhub/multiclusterhub_controller.go
+++ pkg/controller/multiclusterhub/multiclusterhub_controller.go
@@ -48,6 +48,7 @@ import (
 const hubFinalizer = "finalizer.operators.open-cluster-management.io"
 
 var log = logf.Log.WithName("controller_multiclusterhub")
+var resyncPeriod = time.Second * 20
 
 /**
 * USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
@@ -397,7 +398,7 @@ func (r *ReconcileMultiClusterHub) Reconcile(request reconcile.Request) (reconci
 
 	if !ready {
 		// Keep reconciling while install is not complete
-		return reconcile.Result{RequeueAfter: time.Second * 20}, nil
+		return reconcile.Result{RequeueAfter: resyncPeriod}, nil
 	}
 	return reconcile.Result{}, nil
 }
@@ -408,7 +409,7 @@ func (r *ReconcileMultiClusterHub) UpdateStatus(m *operatorsv1beta1.MultiCluster
 		if errors.IsConflict(err) {
 			// Error from object being modified is normal behavior and should not be treated like an error
 			log.Info("Failed to update status", "Reason", "Object has been modified")
-			return &reconcile.Result{RequeueAfter: time.Second * 15}, nil
+			return &reconcile.Result{RequeueAfter: resyncPeriod}, nil
 		}
 
 		log.Error(err, fmt.Sprintf("Failed to update %s/%s status ", m.Namespace, m.Name))
