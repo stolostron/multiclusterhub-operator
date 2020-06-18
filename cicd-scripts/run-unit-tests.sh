f6f29d3955975610e4ee8be5fb04cdea01de0594
Zachary Kayyali <zkayyali@redhat.com>
2020-06-18 14:26:57 -0400

    add license

--- cicd-scripts/run-unit-tests.sh
+++ cicd-scripts/run-unit-tests.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -e
-###############################################################################
-# (c) Copyright IBM Corporation 2019, 2020. All Rights Reserved.
-# Note to U.S. Government Users Restricted Rights:
-# U.S. Government Users Restricted Rights - Use, duplication or disclosure restricted by GSA ADP Schedule
-# Contract with IBM Corp.
-# Licensed Materials - Property of IBM
 # Copyright (c) 2020 Red Hat, Inc.
-###############################################################################
 
 _script_dir=$(dirname "$0")
 mkdir -p test/coverage
