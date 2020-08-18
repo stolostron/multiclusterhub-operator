b45433c23ee5c23d9a309589a47a28a671227abc
Zachary Kayyali <zkayyali@redhat.com>
2020-08-18 16:07:22 -0400

    Install test

--- test/multiclusterhub_install_test/multiclusterhub_test.go
+++ test/multiclusterhub_install_test/multiclusterhub_test.go
@@ -8,6 +8,7 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	utils "github.com/open-cluster-management/multicloudhub-operator/test/utils"
@@ -17,9 +18,9 @@ var _ = Describe("Multiclusterhub", func() {
 
 	BeforeEach(func() {
 		By("Attempting to delete MultiClusterHub if it exists")
-		// utils.DeleteIfExists(utils.DynamicKubeClient, utils.GVRMultiClusterHub, utils.MCHName, utils.MCHNamespace, true)
+		utils.DeleteIfExists(utils.DynamicKubeClient, utils.GVRMultiClusterHub, utils.MCHName, utils.MCHNamespace, true)
 
-		// Expect(utils.ValidateDelete(utils.DynamicKubeClient)).Should(BeNil())
+		Expect(utils.ValidateDelete(utils.DynamicKubeClient)).Should(BeNil())
 	})
 
 	if os.Getenv("full_test_suite") == "true" {
@@ -36,10 +37,10 @@ var _ = Describe("Multiclusterhub", func() {
 
 func FullInstallTestSuite() {
 
-	It("Testing Image Overrides Configmap Sad Cases", func() {
-		// By("Creating MultiClusterHub")
-		// err := utils.ValidateMCH(utils.CreateDefaultMCH())
-		// Expect(err).To(BeNil())
+	It("Testing Image Overrides Configmap", func() {
+		By("- If configmap is manually overwitten, ensure MCH Operator will overwrite")
+		err := utils.ValidateMCH(utils.CreateDefaultMCH())
+		Expect(err).To(BeNil())
 
 		By("- Overrwite Image Overrides Configmap")
 		currentVersion, err := utils.GetCurrentVersionFromMCH()
@@ -51,35 +52,94 @@ func FullInstallTestSuite() {
 		configmap.Data = make(map[string]string)
 		configmap, err = utils.KubeClient.CoreV1().ConfigMaps(utils.MCHNamespace).Update(context.TODO(), configmap, metav1.UpdateOptions{})
 		Expect(err).To(BeNil())
-
 		Expect(len(configmap.Data)).Should(Equal(0))
 
-		configmap, err = utils.KubeClient.CoreV1().ConfigMaps(utils.MCHNamespace).Get(context.TODO(), fmt.Sprintf("mch-image-manifest-%s", currentVersion), metav1.GetOptions{})
-		Expect(len(configmap.Data)).ShouldNot(Equal(0))
+		Eventually(func() error {
+			configmap, err = utils.KubeClient.CoreV1().ConfigMaps(utils.MCHNamespace).Get(context.TODO(), fmt.Sprintf("mch-image-manifest-%s", currentVersion), metav1.GetOptions{})
+			if len(configmap.Data) == 0 {
+				return fmt.Errorf("Configmap has not been updated")
+			}
+			return nil
+		}, 30, 1).Should(BeNil())
+
+		By("- If `mch-imageOverridesCM` annotation is given, ensure Image Overrides Configmap is updated ")
 
 		return
 	})
 
-	// It("Installing MCH with bad pull secret - should have Pending status", func() {
-	// 	By("Creating MultiClusterHub")
-	// 	err := utils.ValidateMCHUnsuccessful(utils.CreateMCHBadPullSecret())
-	// 	if err != nil {
-	// 		fmt.Println(fmt.Sprintf("Error: %s\n", err.Error()))
-	// 		return
-	// 	}
-	// 	return
-	// })
+	It("- If `mch-imageOverridesCM` annotation is given, ensure Image Overrides Configmap is updated ", func() {
+		By("- Creating Developer Image Overrides Configmap")
 
-	// totalAttempts := 10
-	// for i := 1; i <= totalAttempts; i++ {
-	// 	It(fmt.Sprintf("Installing MCH - Attempt %d of %d", i, totalAttempts), func() {
-	// 		By("Creating MultiClusterHub")
-	// 		err := utils.ValidateMCH(utils.CreateDefaultMCH())
-	// 		if err != nil {
-	// 			fmt.Println(fmt.Sprintf("Error: %s\n", err.Error()))
-	// 			return
-	// 		}
-	// 		return
-	// 	})
-	// }
+		err := utils.ValidateMCH(utils.CreateDefaultMCH())
+		Expect(err).To(BeNil())
+
+		configmap := &corev1.ConfigMap{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "my-config",
+				Namespace: utils.MCHNamespace,
+			},
+			Data: map[string]string{
+				"overrides.json": `[
+					{
+					  "image-name": "application-ui",
+					  "image-tag": "not-a-real-tag",
+					  "image-remote": "quay.io/open-cluster-management",
+					  "image-key": "application_ui"
+					}
+				  ]`,
+			},
+		}
+
+		// Create configmap overrides
+		currentVersion, err := utils.GetCurrentVersionFromMCH()
+		Expect(err).To(BeNil())
+		_, err = utils.KubeClient.CoreV1().ConfigMaps(utils.MCHNamespace).Create(context.TODO(), configmap, metav1.CreateOptions{})
+		Expect(err).To(BeNil())
+
+		// Annotate MCH
+		annotations := make(map[string]string)
+		annotations["mch-imageOverridesCM"] = "my-config"
+		mch, err := utils.DynamicKubeClient.Resource(utils.GVRMultiClusterHub).Namespace(utils.MCHNamespace).Get(context.TODO(), utils.MCHName, metav1.GetOptions{})
+		Expect(err).To(BeNil())
+		mch.SetAnnotations(annotations)
+		mch, err = utils.DynamicKubeClient.Resource(utils.GVRMultiClusterHub).Namespace(utils.MCHNamespace).Update(context.TODO(), mch, metav1.UpdateOptions{})
+		Expect(err).To(BeNil())
+
+		configmap, err = utils.KubeClient.CoreV1().ConfigMaps(utils.MCHNamespace).Get(context.TODO(), fmt.Sprintf("mch-image-manifest-%s", currentVersion), metav1.GetOptions{})
+		Expect(len(configmap.Data)).ShouldNot(Equal(0))
+		Expect(configmap.Data["application_ui"]).Should(Equal("quay.io/open-cluster-management/application-ui:not-a-real-tag"))
+
+		annotations = make(map[string]string)
+		mch.SetAnnotations(annotations)
+		_, err = utils.DynamicKubeClient.Resource(utils.GVRMultiClusterHub).Namespace(utils.MCHNamespace).Update(context.TODO(), mch, metav1.UpdateOptions{})
+		Expect(err).To(BeNil())
+
+		err = utils.KubeClient.CoreV1().ConfigMaps(utils.MCHNamespace).Delete(context.TODO(), "my-config", metav1.DeleteOptions{})
+		Expect(err).To(BeNil())
+
+		return
+	})
+
+	It("Installing MCH with bad pull secret - should have Pending status", func() {
+		By("Creating MultiClusterHub")
+		err := utils.ValidateMCHUnsuccessful(utils.CreateMCHBadPullSecret())
+		if err != nil {
+			fmt.Println(fmt.Sprintf("Error: %s\n", err.Error()))
+			return
+		}
+		return
+	})
+
+	totalAttempts := 10
+	for i := 1; i <= totalAttempts; i++ {
+		It(fmt.Sprintf("Installing MCH - Attempt %d of %d", i, totalAttempts), func() {
+			By("Creating MultiClusterHub")
+			err := utils.ValidateMCH(utils.CreateDefaultMCH())
+			if err != nil {
+				fmt.Println(fmt.Sprintf("Error: %s\n", err.Error()))
+				return
+			}
+			return
+		})
+	}
 }
