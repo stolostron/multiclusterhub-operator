d5e590a73086f65a53492363d97feb491c7f9337
Nathan Weatherly <60662528+nathanweatherly@users.noreply.github.com>
2020-08-21 11:36:43 -0400

    refactoring to check all statuses in single function (#359)

--- test/multiclusterhub_install_test/multiclusterhub_test.go
+++ test/multiclusterhub_install_test/multiclusterhub_test.go
@@ -25,7 +25,8 @@ var _ = Describe("Multiclusterhub", func() {
 
 		It(fmt.Sprintf("Installing MCH with bad pull secret - should have Pending status"), func() {
 			By("Creating MultiClusterHub")
-			err := utils.ValidateMCHUnsuccessful(utils.CreateMCHBadPullSecret())
+			utils.CreateMCHBadPullSecret()
+			err := utils.ValidateMCHUnsuccessful()
 			if err != nil {
 				fmt.Println(fmt.Sprintf("Error: %s\n", err.Error()))
 				return
@@ -37,17 +38,17 @@ var _ = Describe("Multiclusterhub", func() {
 		for i := 1; i <= totalAttempts; i++ {
 			ok := It(fmt.Sprintf("Installing MCH - Attempt %d of %d", i, totalAttempts), func() {
 				By("Creating MultiClusterHub")
-					mch := utils.CreateDefaultMCH()
-					if err := utils.ValidateComponentStatusExist(mch); err != nil {
-						fmt.Println(fmt.Sprintf("Error: %s\n", err.Error()))
-						return
-					}
-					err := utils.ValidateMCH(mch)
-					if err != nil {
-						fmt.Println(fmt.Sprintf("Error: %s\n", err.Error()))
-						return
-					}
+				utils.CreateDefaultMCH()
+				if err := utils.ValidateStatusesExist(); err != nil {
+					fmt.Println(fmt.Sprintf("Error: %s\n", err.Error()))
 					return
+				}
+				err := utils.ValidateMCH()
+				if err != nil {
+					fmt.Println(fmt.Sprintf("Error: %s\n", err.Error()))
+					return
+				}
+				return
 			})
 			if !ok {
 				break
@@ -57,12 +58,12 @@ var _ = Describe("Multiclusterhub", func() {
 		By("Beginning Basic Install Test Suite ...")
 		It("Install Default MCH CR", func() {
 			By("Creating MultiClusterHub")
-			mch := utils.CreateDefaultMCH()
-			if err := utils.ValidateComponentStatusExist(mch); err != nil {
+			utils.CreateDefaultMCH()
+			if err := utils.ValidateStatusesExist(); err != nil {
 				fmt.Println(fmt.Sprintf("Error: %s\n", err.Error()))
 				return
 			}
-			err := utils.ValidateMCH(mch)
+			err := utils.ValidateMCH()
 			if err != nil {
 				fmt.Println(fmt.Sprintf("Error: %s\n", err.Error()))
 				return
