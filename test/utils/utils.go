d3c3d13e9cc88672e9821a5a0c314701a606c878
Nathan Weatherly <60662528+nathanweatherly@users.noreply.github.com>
2021-01-20 15:53:09 -0500

    bump version to 2.1.3 (#467)
    * bump version to 2.1.3
    
    * sc caught a new file, added to update version list

--- test/utils/utils.go
+++ test/utils/utils.go
@@ -740,7 +740,7 @@ func ValidateMCH() error {
 	Expect(err).Should(BeNil())
 	v, err := semver.NewVersion(currentVersion)
 	Expect(err).Should(BeNil())
-	c, err := semver.NewConstraint(">= 2.1.2")
+	c, err := semver.NewConstraint(">= 2.1.3")
 	Expect(err).Should(BeNil())
 	if c.Check(v) {
 		By("- Ensuring image manifest configmap is created")
