a7b2359734e191d01f5b356721c2cf84e1baa970
Nathan Weatherly <60662528+nathanweatherly@users.noreply.github.com>
2021-01-21 11:08:31 -0500

    local copy of doc-ed clean-up script (#466)

--- test/clean-up.sh
+++ test/clean-up.sh
@@ -9,21 +9,17 @@ else
     echo "Cleaning up resources in namespace: $ACM_NAMESPACE"
 fi
 
-
 oc delete mch --all -n $ACM_NAMESPACE
-
-## Delete all helm charts in given namespace
 helm ls --namespace $ACM_NAMESPACE | cut -f 1 | tail -n +2 | xargs -n 1 helm delete --namespace $ACM_NAMESPACE
-
-oc delete apiservice v1.admission.cluster.open-cluster-management.io v1beta1.webhook.certmanager.k8s.io v1.admission.cluster.open-cluster-management.io v1.admission.work.open-cluster-management.io
+oc delete apiservice v1beta1.webhook.certmanager.k8s.io v1.admission.cluster.open-cluster-management.io v1.admission.work.open-cluster-management.io
 oc delete clusterimageset --all
 oc delete configmap -n $ACM_NAMESPACE cert-manager-controller cert-manager-cainjector-leader-election cert-manager-cainjector-leader-election-core
 oc delete consolelink acm-console-link
-oc delete crd klusterletaddonconfigs.agent.open-cluster-management.io placementbindings.policy.open-cluster-management.io policies.policy.open-cluster-management.io userpreferences.console.open-cluster-management.io
-oc delete mutatingwebhookconfiguration cert-manager-webhook-v1alpha1
+oc delete crd klusterletaddonconfigs.agent.open-cluster-management.io placementbindings.policy.open-cluster-management.io policies.policy.open-cluster-management.io userpreferences.console.open-cluster-management.io searchservices.search.acm.com
+oc delete mutatingwebhookconfiguration cert-manager-webhook cert-manager-webhook-v1alpha1
 oc delete oauthclient multicloudingress
 oc delete rolebinding -n kube-system cert-manager-webhook-webhook-authentication-reader
 oc delete scc kui-proxy-scc
-oc delete validatingwebhookconfiguration cert-manager-webhook-v1alpha1
+oc delete validatingwebhookconfiguration cert-manager-webhook cert-manager-webhook-v1alpha1
 
 exit 0
