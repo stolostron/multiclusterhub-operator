a3b1978f1d46f9a436cf18b47a80f6d1e196bc38
Cameron Wall <cwall@redhat.com>
2021-03-17 12:58:59 -0400

    delete rcm on update

--- pkg/controller/multiclusterhub/uninstall.go
+++ pkg/controller/multiclusterhub/uninstall.go
@@ -28,6 +28,10 @@ var (
 				types.NamespacedName{Name: "topology-sub", Namespace: m.Namespace},
 				schema.GroupVersionKind{Group: "apps.open-cluster-management.io", Kind: "Subscription", Version: "v1"},
 			),
+			newUnstructured(
+				types.NamespacedName{Name: "rcm-sub", Namespace: m.Namespace},
+				schema.GroupVersionKind{Group: "apps.open-cluster-management.io", Kind: "Subscription", Version: "v1"},
+			),
 			// searchservices CRD replaced in 2.2.0
 			newUnstructured(
 				types.NamespacedName{Name: "searchservices.search.acm.com"},
