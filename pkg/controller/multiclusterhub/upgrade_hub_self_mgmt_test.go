b8499fa56ed28781491b2253a396726f48f890ef
Jakob Gray <20209054+JakobGray@users.noreply.github.com>
2021-05-27 08:51:53 -0400

    Update to version 2.1.8

--- pkg/controller/multiclusterhub/upgrade_hub_self_mgmt_test.go
+++ pkg/controller/multiclusterhub/upgrade_hub_self_mgmt_test.go
@@ -18,7 +18,7 @@ import (
 
 func Test_getImageFromManifestByKey(t *testing.T) {
 
-	full_mch.Status.DesiredVersion = "2.1.7"
+	full_mch.Status.DesiredVersion = "2.1.8"
 	tests := []struct {
 		Name      string
 		ImageKey  string
