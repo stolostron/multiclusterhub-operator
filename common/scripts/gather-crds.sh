32a3cb4a7f5d88ce888e9f003ac946d671695e3c
Nathan Weatherly <nweather@redhat.com>
2021-07-06 12:32:53 -0400

    rename that works
    Signed-off-by: Nathan Weatherly <nweather@redhat.com>

--- common/scripts/gather-crds.sh
+++ common/scripts/gather-crds.sh
@@ -22,5 +22,5 @@ rm -rf crd-temp
 curl  -o './crds/0000_01_addon.open-cluster-management.io_managedclusteraddons.crd.yaml' -LJO https://raw.githubusercontent.com/open-cluster-management/api/main/addon/v1alpha1/0000_01_addon.open-cluster-management.io_managedclusteraddons.crd.yaml
 
 curl  -o './crds/0000_00_addon.open-cluster-management.io_clustermanagementaddons.crd.yaml' -LJO https://raw.githubusercontent.com/open-cluster-management/api/main/addon/v1alpha1/0000_00_addon.open-cluster-management.io_clustermanagementaddons.crd.yaml
-mv "./crds/0000_00_addon.open-cluster-management.io_clustermanagementaddons.crd.yaml -LJO" ./crds/0000_00_addon.open-cluster-management.io_clustermanagementaddons.crd.yaml
+mv ./crds/0000_00_addon.open-cluster-management.io_clustermanagementaddons.crd.yam* ./crds/0000_00_addon.open-cluster-management.io_clustermanagementaddons.crd.yaml
 ls ./crds
