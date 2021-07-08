98b909c3d84d9faf69b0ed62632f96d64762114e
Zachary Kayyali <zkayyali@redhat.com>
2021-07-08 15:16:08 -0400

    Fix Mock PR Function tests
    Signed-off-by: Zachary Kayyali <zkayyali@redhat.com>

--- mock-component-image/scripts/generate-mock-foundation-binaries.sh
+++ mock-component-image/scripts/generate-mock-foundation-binaries.sh
@@ -16,3 +16,6 @@ mv foundation-webhook ./bin/webhook
 
 GOOS=linux go build ./templates/foundation-proxyserver.go
 mv foundation-proxyserver ./bin/proxyserver
+
+curl -o bin/kubectl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x bin/kubectl
--- mock-component-image/templates/foundation-controller.go
+++ mock-component-image/templates/foundation-controller.go
@@ -6,6 +6,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os/exec"
 )
 
 func MockPing(w http.ResponseWriter, r *http.Request) {
@@ -15,5 +16,16 @@ func MockPing(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/healthz", MockPing)
 	http.HandleFunc("/readyz", MockPing)
+
+	cmd := exec.Command("./bin/kubectl", "apply", "-k", "templates/resources/")
+	stdout, err := cmd.Output()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	// Print the output
+	fmt.Println(string(stdout))
+
 	http.ListenAndServe(":8000", nil)
 }
