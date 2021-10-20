858c2c001f8ac9d39f6066ff102c761dd85fc669
Jakob Gray <20209054+JakobGray@users.noreply.github.com>
2021-10-20 15:48:05 -0400

    Update to v2.1.13
    Signed-off-by: Jakob Gray <20209054+JakobGray@users.noreply.github.com>

--- pkg/controller/multiclusterhub/upgrade_hub_self_mgmt_test.go
+++ pkg/controller/multiclusterhub/upgrade_hub_self_mgmt_test.go
@@ -18,7 +18,7 @@ import (
 
 func Test_getImageFromManifestByKey(t *testing.T) {
 
-	full_mch.Status.DesiredVersion = "2.1.12"
+	full_mch.Status.DesiredVersion = "2.1.13"
 	tests := []struct {
 		Name      string
 		ImageKey  string
