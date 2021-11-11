1cb95a1e2d3b3410328b478b261dc6b9518907eb
Jakob Gray <20209054+JakobGray@users.noreply.github.com>
2021-11-11 15:03:48 -0500

    Update to v2.4.1
    Signed-off-by: Jakob Gray <20209054+JakobGray@users.noreply.github.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -4,5 +4,5 @@
 package version
 
 var (
-	Version = "2.4.0"
+	Version = "2.4.1"
 )
--- test/function_tests/multiclusterhub_install_test/multiclusterhub_test.go
+++ test/function_tests/multiclusterhub_install_test/multiclusterhub_test.go
@@ -161,7 +161,7 @@ func FullInstallTestSuite() {
 		Expect(err).To(BeNil())
 		v, err := semver.NewVersion(currentVersion)
 		Expect(err).Should(BeNil())
-		c, err := semver.NewConstraint(">= 2.4.0")
+		c, err := semver.NewConstraint(">= 2.4.1")
 		Expect(err).Should(BeNil())
 
 		if c.Check(v) {
@@ -216,7 +216,7 @@ func FullInstallTestSuite() {
 
 		v, err := semver.NewVersion(currentVersion)
 		Expect(err).Should(BeNil())
-		c, err := semver.NewConstraint(">= 2.4.0")
+		c, err := semver.NewConstraint(">= 2.4.1")
 		Expect(err).Should(BeNil())
 		if c.Check(v) {
 			_, err = utils.KubeClient.CoreV1().ConfigMaps(utils.MCHNamespace).Create(context.TODO(), configmap, metav1.CreateOptions{})
