bbf53c7c3199e9e66088af050ddb35e4db0e0baa
Cameron Wall <75807506+cameronmwall@users.noreply.github.com>
2023-04-04 17:06:10 -0400

    Implement Leader Collection (#1008)
    Signed-off-by: Cameron Wall <cwall@redhat.com>
    Signed-off-by: Xiangjing Li <xiangli@redhat.com>

--- main.go
+++ main.go
@@ -126,11 +126,27 @@ func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
 	var probeAddr string
+	var leaseDuration time.Duration
+	var renewDeadline time.Duration
+	var retryPeriod time.Duration
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8383", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", true,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
+			flag.DurationVar(&leaseDuration, "leader-election-lease-duration", 137*time.Second, ""+
+			"The duration that non-leader candidates will wait after observing a leadership "+
+			"renewal until attempting to acquire leadership of a led but unrenewed leader "+
+			"slot. This is effectively the maximum duration that a leader can be stopped "+
+			"before it is replaced by another candidate. This is only applicable if leader "+
+			"election is enabled.")
+		flag.DurationVar(&renewDeadline, "leader-election-renew-deadline", 107*time.Second, ""+
+			"The interval between attempts by the acting master to renew a leadership slot "+
+			"before it stops leading. This must be less than or equal to the lease duration. "+
+			"This is only applicable if leader election is enabled.")
+		flag.DurationVar(&retryPeriod, "leader-election-retry-period", 26*time.Second, ""+
+			"The duration the clients should wait between attempting acquisition and renewal "+
+			"of a leadership. This is only applicable if leader election is enabled.")
 	opts := zap.Options{
 		Development: true,
 	}
@@ -154,6 +170,9 @@ func main() {
 		LeaderElectionID:        "multicloudhub-operator-lock",
 		WebhookServer:           &ctrlwebhook.Server{TLSMinVersion: "1.2"},
 		LeaderElectionNamespace: ns,
+		LeaseDuration:          &leaseDuration,
+		RenewDeadline:          &renewDeadline,
+		RetryPeriod:            &retryPeriod,
 	}
 
 	cacheSecrets := os.Getenv(NoCacheEnv)
