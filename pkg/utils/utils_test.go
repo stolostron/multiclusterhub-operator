d53524ccab0a3cea4cac488339359351796ec0b4
Erin Murphy <42587690+eemurphy@users.noreply.github.com>
2023-08-11 16:56:11 -0400

    cluster-permission operator install (#1058)
    * cluster-permission operator install
    
    Signed-off-by: Erin Murphy erinmurp@redhat.com
    
    * python script fix
    
    Signed-off-by: Erin Murphy erinmurp@redhat.com
    
    * fix sonar tests
    
    Signed-off-by: Erin Murphy erinmurp@redhat.com
    
    * test coverage
    
    Signed-off-by: Cameron Wall <cwall@redhat.com>
    
    fix merge conflicts
    
    Signed-off-by: Cameron Wall <cwall@redhat.com>
    
    ---------
    
    Signed-off-by: Erin Murphy erinmurp@redhat.com
    Signed-off-by: Cameron Wall <cwall@redhat.com>
    Co-authored-by: Cameron Wall <cwall@redhat.com>
    Signed-off-by: Xiangjing Li <xiangli@redhat.com>

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -228,6 +228,12 @@ var _ = Describe("utility functions", func() {
 			d := GetDeploymentsForStatus(&mch, true)
 			Expect(len(d)).To(Equal(1))
 		})
+		It("gets deployments for status with cluster-permission enabled", func() {
+			mch := resources.EmptyMCH()
+			mch.Enable(mchv1.ClusterPermission)
+			d := GetDeploymentsForStatus(&mch, true)
+			Expect(len(d)).To(Equal(1))
+		})
 		It("Sets Default Component values", func() {
 			mch := resources.EmptyMCH()
 			updated, err := SetDefaultComponents(&mch)
