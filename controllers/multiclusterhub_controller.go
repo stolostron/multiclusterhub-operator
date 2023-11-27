aa44c5e051ad970992416d268e2fb5f16d6e654f
Nathaniel Graham <[nathaniel.graham@protonmail.com, ngraham@redhat.com]>
2023-11-27 10:36:19 -0700

    Removed CRD guard
    Signed-off-by: Nathaniel Graham <[nathaniel.graham@protonmail.com, ngraham@redhat.com]>

--- controllers/multiclusterhub_controller.go
+++ controllers/multiclusterhub_controller.go
@@ -693,14 +693,12 @@ func (r *MultiClusterHubReconciler) ensureComponent(ctx context.Context, m *oper
 
 	// Applies all templates
 	for _, template := range templates {
-		if template.GetKind() != "CustomResourceDefinition" {
-			annotations := template.GetAnnotations()
-			if annotations == nil {
-				annotations = make(map[string]string)
-			}
-			annotations[utils.AnnotationReleaseVersion] = version.Version
-			template.SetAnnotations(annotations)
+		annotations := template.GetAnnotations()
+		if annotations == nil {
+			annotations = make(map[string]string)
 		}
+		annotations[utils.AnnotationReleaseVersion] = version.Version
+		template.SetAnnotations(annotations)
 		result, err := r.applyTemplate(ctx, m, template)
 		if err != nil {
 			return result, err
