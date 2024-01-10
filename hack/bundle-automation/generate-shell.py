53e665b8efab2e9255372813368df7a86ed96f20
Cameron Wall <cwall@redhat.com>
2024-01-10 13:15:49 -0500

    bundle generation move with config
    Signed-off-by: Cameron Wall <cwall@redhat.com>

--- hack/bundle-automation/generate-shell.py
+++ hack/bundle-automation/generate-shell.py
@@ -3,11 +3,11 @@ import os
 import shutil
 
 
-repo_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "tmp/bundle-gen") # Path to clone repo to
+repo_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "tmp/dev-tools") # Path to clone repo to
 if os.path.exists(repo_path): # If path exists, remove and re-clone
     shutil.rmtree(repo_path)
 repository = Repo.clone_from("https://github.com/stolostron/installer-dev-tools.git", repo_path) # Clone repo to above path
 repository.git.checkout('main') # If a branch is specified, checkout that branch
-
-os.system("python3 " + repo_path +  "/bundle-generation/generate-bundles.py --destination pkg/templates/ --configLocation MCHconfig.yaml")
-shutil.rmtree((os.path.join(os.path.dirname(os.path.realpath(__file__)), "tmp")), ignore_errors=True)       
+shutil.copy(os.path.join(os.path.dirname(os.path.realpath(__file__)), "tmp/dev-tools/bundle-generation/generate-bundles.py"), os.path.join(os.path.dirname(os.path.realpath(__file__)), "generate-bundles.py"))
+os.system("python3 " + os.path.dirname(os.path.realpath(__file__)) +  "/generate-bundles.py --destination pkg/templates/")
+os.remove(os.path.join(os.path.dirname(os.path.realpath(__file__)), "generate-bundles.py"))       
