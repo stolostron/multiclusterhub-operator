0dae2bcf929cdfe18a870cdc363b5e16c087caad
Nathaniel Graham <[nathaniel.graham@protonmail.com, ngraham@redhat.com]>
2024-01-25 17:51:29 -0700

    updated unit test
    Signed-off-by: Nathaniel Graham <[nathaniel.graham@protonmail.com, ngraham@redhat.com]>

--- pkg/multiclusterengine/multiclusterengine_test.go
+++ pkg/multiclusterengine/multiclusterengine_test.go
@@ -329,7 +329,6 @@ func TestNewMultiClusterEngine(t *testing.T) {
 						},
 					},
 				},
-				infrastructureCustomNamespace: "open-cluster-management",
 			},
 			want: &mcev1.MultiClusterEngine{
 				ObjectMeta: metav1.ObjectMeta{
@@ -360,7 +359,6 @@ func TestNewMultiClusterEngine(t *testing.T) {
 							{Name: operatorsv1.MCEDiscovery, Enabled: false},
 							{Name: operatorsv1.MCELocalCluster, Enabled: false},
 						},
-						InfrastructureCustomNamespace: "open-cluster-management",
 					},
 				},
 			},
@@ -377,8 +375,6 @@ func TestNewMultiClusterEngine(t *testing.T) {
 			g.Expect(got.Spec.TargetNamespace).To(gomega.Equal(tt.want.Spec.TargetNamespace))
 			g.Expect(got.Spec.Overrides.Components).To(gomega.Equal(tt.want.Spec.Overrides.Components))
 			g.Expect(got.Spec.Overrides.ImagePullPolicy).To(gomega.Equal(tt.want.Spec.Overrides.ImagePullPolicy))
-			g.Expect(got.Spec.Overrides.InfrastructureCustomNamespace).To(gomega.Equal(tt.want.Spec.Overrides.InfrastructureCustomNamespace))
-
 		})
 	}
 }
