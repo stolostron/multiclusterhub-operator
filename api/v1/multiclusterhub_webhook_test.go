8c9dee3509607130df2507f3bbd6bd4e59b0dd99
Nathaniel Graham <ngraham@redhat.com>
2024-04-10 15:13:16 -0600

    Added create before delete again
    Signed-off-by: Nathaniel Graham <ngraham@redhat.com>

--- api/v1/multiclusterhub_webhook_test.go
+++ api/v1/multiclusterhub_webhook_test.go
@@ -136,6 +136,7 @@ var _ = Describe("Multiclusterhub webhook", func() {
 						Annotations: map[string]string{"deploymentmode": string(ModeHosted)},
 					},
 				}
+				Expect(k8sClient.Create(ctx, mch)).Should(Succeed())
 				Expect(k8sClient.Get(ctx, types.NamespacedName{Name: multiClusterHubName, Namespace: "default"}, mch)).To(Succeed())
 				Expect(k8sClient.Delete(ctx, mch)).To(BeNil(), "MCH delete was blocked unexpectedly")
 			})
