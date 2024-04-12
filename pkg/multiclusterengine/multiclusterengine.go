dbbd2456b019d6c06028266bc801cf921c2e566a
Nathaniel Graham <ngraham@redhat.com>
2024-04-12 11:33:49 -0600

    Lets try coercing the enum types
    Signed-off-by: Nathaniel Graham <ngraham@redhat.com>

--- pkg/multiclusterengine/multiclusterengine.go
+++ pkg/multiclusterengine/multiclusterengine.go
@@ -77,6 +77,21 @@ func NewMultiClusterEngine(m *operatorsv1.MultiClusterHub) *mcev1.MultiClusterEn
 		availConfig = mcev1.HABasic
 	}
 
+	// There has got to be a cleaner way of converting between enum types
+	// var hubsize mcev1.HubSize
+	// switch m.Spec.HubSize {
+	// case operatorsv1.Small:
+	// 	hubsize = mcev1.Small
+	// case operatorsv1.Medium:
+	// 	hubsize = mcev1.Medium
+	// case operatorsv1.Large:
+	// 	hubsize = mcev1.Large
+	// case operatorsv1.ExtraLarge:
+	// 	hubsize = mcev1.ExtraLarge
+	// default:
+	// 	hubsize = mcev1.Medium
+	// }
+
 	mce := &mcev1.MultiClusterEngine{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:        MulticlusterengineName,
@@ -89,6 +104,7 @@ func NewMultiClusterEngine(m *operatorsv1.MultiClusterHub) *mcev1.MultiClusterEn
 			NodeSelector:       m.Spec.NodeSelector,
 			AvailabilityConfig: availConfig,
 			TargetNamespace:    OperandNameSpace(),
+			HubSize:            mcev1.HubSize(m.Spec.HubSize),
 			Overrides: &mcev1.Overrides{
 				Components: utils.GetMCEComponents(m),
 			},
