9cfd3ed75db4e4ee6819c8f091cceed0369dd7ee
Sahar Ebrahimi <sahare@redhat.com>
2024-05-31 10:23:05 -0400

    Fix the issues related to minimum OADP channel value (#1532)
    * Fix the quotes issue for .Values.global.channel when parsed by helm
    
    Signed-off-by: sahare <sahare@redhat.com>
    
    * Read the expected OADP channel from oadpOperator section in the values.yaml
    
    Signed-off-by: sahare <sahare@redhat.com>
    
    * Correct the indentation
    
    Signed-off-by: sahare <sahare@redhat.com>
    
    * Add minOADPChannel to global vars, remove unused oadpOperator vars
    
    Signed-off-by: sahare <sahare@redhat.com>
    
    * update to set global.MinOADPChannel
    
    - also organize into oadp default constants
    
    Signed-off-by: Tesshu Flower <tflower@redhat.com>
    
    ---------
    
    Signed-off-by: sahare <sahare@redhat.com>
    Signed-off-by: Tesshu Flower <tflower@redhat.com>
    Co-authored-by: Tesshu Flower <tflower@redhat.com>

--- pkg/rendering/renderer.go
+++ pkg/rendering/renderer.go
@@ -39,6 +39,7 @@ type Global struct {
 	ImageRepository     string               `json:"imageRepository" structs:"namespace"`
 	Name                string               `json:"name" structs:"name"`
 	Channel             string               `json:"channel" structs:"Channel"`
+	MinOADPChannel      string               `json:"minOADPChannel" structs:"minOADPChannel"`
 	InstallPlanApproval subv1alpha1.Approval `json:"installPlanApproval" structs:"installPlanApproval"`
 	Source              string               `json:"source" structs:"source"`
 	SourceNamespace     string               `json:"sourceNamespace" structs:"sourceNamespace"`
@@ -64,6 +65,15 @@ type Toleration struct {
 	Effect            corev1.TaintEffect        `json:"Effect" protobuf:"bytes,4,opt,name=effect,casttype=TaintEffect"`
 	TolerationSeconds *int64                    `json:"TolerationSeconds" protobuf:"varint,5,opt,name=tolerationSeconds"`
 }
+
+// defaults for the OADP subscription that will be created by the installer
+const (
+	defaultOADPChannel         = "stable-1.3" // This will also be the minOADPChannel (min version we expect to be installed)
+	defaultOADPName            = "redhat-oadp-operator"
+	defaultOADPInstallPlan     = "Automatic"
+	defaultOADPSource          = "redhat-operators"
+	defaultOADPSourceNamespace = "openshift-marketplace"
+)
 
 var log = logf.Log.WithName("reconcile")
 
@@ -347,6 +357,8 @@ func injectValuesOverrides(values *Values, mch *v1.MultiClusterHub, images map[s
 
 	values.Global.Name, values.Global.Channel, values.Global.InstallPlanApproval, values.Global.Source, values.Global.SourceNamespace = GetOADPConfig(mch)
 
+	values.Global.MinOADPChannel = defaultOADPChannel
+
 	// TODO: Define all overrides
 }
 
@@ -367,31 +379,31 @@ func GetOADPConfig(m *v1.MultiClusterHub) (string, string, subv1alpha1.Approval,
 	if sub.Package != "" {
 		name = sub.Package
 	} else {
-		name = "redhat-oadp-operator"
+		name = defaultOADPName
 	}
 
 	if sub.Channel != "" {
 		channel = sub.Channel
 	} else {
-		channel = "stable-1.3"
+		channel = defaultOADPChannel
 	}
 
 	if sub.InstallPlanApproval != "" {
 		installPlan = sub.InstallPlanApproval
 	} else {
-		installPlan = "Automatic"
+		installPlan = defaultOADPInstallPlan
 	}
 
 	if sub.CatalogSource != "" {
 		source = sub.CatalogSource
 	} else {
-		source = "redhat-operators"
+		source = defaultOADPSource
 	}
 
 	if sub.CatalogSourceNamespace != "" {
 		sourceNamespace = sub.CatalogSourceNamespace
 	} else {
-		sourceNamespace = "openshift-marketplace"
+		sourceNamespace = defaultOADPSourceNamespace
 	}
 	return name, channel, installPlan, source, sourceNamespace
 }
