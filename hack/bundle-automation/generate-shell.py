4b3c2ebca824e1b36f0ebafc643f0515ff7412ec
Nathaniel Graham <ngraham@redhat.com>
2024-06-17 14:31:04 -0600

    renamed generate-bundles.py to charts-to-bundles.py
    Signed-off-by: Nathaniel Graham <ngraham@redhat.com>

--- hack/bundle-automation/generate-shell.py
+++ hack/bundle-automation/generate-shell.py
@@ -8,6 +8,6 @@ if os.path.exists(repo_path): # If path exists, remove and re-clone
     shutil.rmtree(repo_path)
 repository = Repo.clone_from("https://github.com/stolostron/installer-dev-tools.git", repo_path) # Clone repo to above path
 repository.git.checkout('main') # If a branch is specified, checkout that branch
-shutil.copy(os.path.join(os.path.dirname(os.path.realpath(__file__)), "tmp/dev-tools/bundle-generation/generate-bundles.py"), os.path.join(os.path.dirname(os.path.realpath(__file__)), "generate-bundles.py"))
-os.system("python3 " + os.path.dirname(os.path.realpath(__file__)) +  "/generate-bundles.py --destination pkg/templates/")
-os.remove(os.path.join(os.path.dirname(os.path.realpath(__file__)), "generate-bundles.py"))       
+shutil.copy(os.path.join(os.path.dirname(os.path.realpath(__file__)), "tmp/dev-tools/bundle-generation/bundles-to-charts.py"), os.path.join(os.path.dirname(os.path.realpath(__file__)), "generate-bundles.py"))
+os.system("python3 " + os.path.dirname(os.path.realpath(__file__)) +  "/bundles-to-charts.py --destination pkg/templates/")
+os.remove(os.path.join(os.path.dirname(os.path.realpath(__file__)), "bundles-to-charts.py"))       
