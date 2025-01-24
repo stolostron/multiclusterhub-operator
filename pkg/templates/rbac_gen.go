88c02f9cbb39729bcd8014cf459b3a4680163b68
Justin Kulikauskas <jkulikau@redhat.com>
2025-01-23 23:42:13 -0500

    Add InternalHubComponent rbac to the policy-addon
    Refs:
     - https://issues.redhat.com/browse/ACM-14707
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

--- pkg/templates/rbac_gen.go
+++ pkg/templates/rbac_gen.go
@@ -197,6 +197,8 @@ package main
 //+kubebuilder:rbac:groups=networking.k8s.io,resources=ingresses,verbs=get;list;create;update;delete;deletecollection;watch
 //+kubebuilder:rbac:groups=oauth.openshift.io,resources=oauthclients,verbs=create;delete;get;list;patch;update;watch
 //+kubebuilder:rbac:groups=observability.open-cluster-management.io,resources=*;multiclusterobservabilities;endpointmonitorings,verbs=create;delete;get;list;patch;update;watch
+//+kubebuilder:rbac:groups=operator.open-cluster-management.io,resources=internalhubcomponents,verbs=get;list;update;watch
+//+kubebuilder:rbac:groups=operator.open-cluster-management.io,resources=internalhubcomponents/finalizers,verbs=update
 //+kubebuilder:rbac:groups=operator.open-cluster-management.io,resources=multiclusterglobalhubs,verbs=get;list
 //+kubebuilder:rbac:groups=operator.open-cluster-management.io,resources=multiclusterhubs,verbs=get
 //+kubebuilder:rbac:groups=operator.open-cluster-management.io,resources=multiclusterhubs,verbs=get;list
