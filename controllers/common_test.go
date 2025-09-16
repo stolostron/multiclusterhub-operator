ffff2f621fa45c1f7e75f37b76a58afa18ab6b31
Cameron Wall <cwall@redhat.com>
2025-09-16 17:29:40 -0400

    unit test
    Signed-off-by: Cameron Wall <cwall@redhat.com>

--- controllers/common_test.go
+++ controllers/common_test.go
@@ -14,7 +14,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
