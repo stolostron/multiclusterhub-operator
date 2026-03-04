f0dfea3980889f5f90eded4a59fc9d72f012ac19
Disaiah Bennett <dbennett@redhat.com>
2026-03-04 12:52:21 -0500

    [release-2.11] Update Go dependencies to resolve CVEs (#3553)
    * Update Go dependencies to resolve CVEs
    
    Resolves CVE-2025-68121, CVE-2025-61726, CVE-2025-61728, CVE-2025-61729, CVE-2025-61730
    
    Updated Go version from 1.21 to 1.24.0 and golang.org/x dependencies to latest versions
    
    Signed-off-by: dislbenn <dbennett@redhat.com>
    
    * Update Dockerfiles to use Go 1.24
    
    Updated builder images from go1.21 to go1.24 to match go.mod version
    
    Signed-off-by: dislbenn <dbennett@redhat.com>
    
    * Add toolchain go1.24.13 to go.mod
    
    Enables automatic download of Go 1.24.13 toolchain for CVE fixes
    
    Signed-off-by: dislbenn <dbennett@redhat.com>
    
    * Fix Go 1.25 vet errors for non-constant format strings
    
    Go 1.25 requires format strings to be constant for security. Fixed:
    - renderer_test.go: Changed t.Logf/Fatalf(err.Error()) to use %s format
    - overrides.go: Removed nested fmt.Sprintf inside fmt.Errorf
    - status_test.go: Changed t.Errorf(err.Error()) to use %s format
    
    Signed-off-by: dislbenn <dbennett@redhat.com>
    
    ---------
    
    Signed-off-by: dislbenn <dbennett@redhat.com>

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/stolostron/multiclusterhub-operator
 
-go 1.21
+go 1.25.0
+
+toolchain go1.25.2
 
 require (
 	github.com/Masterminds/semver/v3 v3.2.1
@@ -85,15 +87,15 @@ require (
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/crypto v0.48.0 // indirect
 	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
-	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/net v0.50.0 // indirect
 	golang.org/x/oauth2 v0.19.0 // indirect
-	golang.org/x/sys v0.28.0 // indirect
-	golang.org/x/term v0.27.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/sys v0.41.0 // indirect
+	golang.org/x/term v0.40.0 // indirect
+	golang.org/x/text v0.34.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
-	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
+	golang.org/x/tools v0.41.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240401170217-c3f982113cda // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240401170217-c3f982113cda // indirect
--- pkg/rendering/renderer_test.go
+++ pkg/rendering/renderer_test.go
@@ -107,7 +107,7 @@ func TestRender(t *testing.T) {
 	templates, errs := RenderCharts(chartsDir, testMCH, testImages, templateOverrides, false)
 	if len(errs) > 0 {
 		for _, err := range errs {
-			t.Logf(err.Error())
+			t.Logf("%s", err.Error())
 		}
 		t.Fatalf("failed to retrieve templates")
 		if len(templates) == 0 {
@@ -120,7 +120,7 @@ func TestRender(t *testing.T) {
 			deployment := &appsv1.Deployment{}
 			err := runtime.DefaultUnstructuredConverter.FromUnstructured(template.Object, deployment)
 			if err != nil {
-				t.Fatalf(err.Error())
+				t.Fatalf("%s", err.Error())
 			}
 
 			selectorEquality := reflect.DeepEqual(deployment.Spec.Template.Spec.NodeSelector, mchNodeSelector)
@@ -182,7 +182,7 @@ func TestRender(t *testing.T) {
 		singleChartTemplates, errs := RenderChart(chartsPath, testMCH, singleChartTestImages, templateOverrides, false)
 		if len(errs) > 0 {
 			for _, err := range errs {
-				t.Logf(err.Error())
+				t.Logf("%s", err.Error())
 			}
 			t.Fatalf("failed to retrieve templates")
 			if len(singleChartTemplates) == 0 {
@@ -194,7 +194,7 @@ func TestRender(t *testing.T) {
 				deployment := &appsv1.Deployment{}
 				err := runtime.DefaultUnstructuredConverter.FromUnstructured(template.Object, deployment)
 				if err != nil {
-					t.Fatalf(err.Error())
+					t.Fatalf("%s", err.Error())
 				}
 
 				selectorEquality := reflect.DeepEqual(deployment.Spec.Template.Spec.NodeSelector, mchNodeSelector)
